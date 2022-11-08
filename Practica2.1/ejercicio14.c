76ec598ce30abe80a6a79b800b0b66b592f590a6
AlexVonex <117632525+AlexVonex@users.noreply.github.com>
2022-11-08 20:40:49 +0100

    Create ejercicio14.c

---
+++ Practica2.1/ejercicio14.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/utsname.h>
+#include <sys/types.h>
+#include <limits.h>
+#include <pwd.h>
+#include <time.h>
+#include <sys/time.h>
+
+
+int main(){ 
+    time_t t = time(NULL);
+    struct tm *a = localtime(&t); 
+    printf("Año: %li\n", 1900 + a->tm_year);
+
+   return 1;
+}
