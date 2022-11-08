ef136b16a3168675ccc952066ae5f5cd8d7953de
AlexVonex <117632525+AlexVonex@users.noreply.github.com>
2022-11-08 19:33:50 +0100

    Update ejercicio7.c

--- Practica2.1/ejercicio7.c
+++ Practica2.1/ejercicio7.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/utsname.h>
+#include <sys/types.h>
+#include <limits.h>
 
+int main(/int argc, int argv/){ 
+    int s;
+
+    printf("Numero maximo de enlaces: %d\n", fpathconf(s,_PC_LINK_MAX));
+    printf("Tamano maximo de una ruta: %ld\n", fpathconf(s,_PC_PATH_MAX));
+    printf("Tamano maximo de un fichero: %ld\n", fpathconf(s ,_PC_NAME_MAX));
+    
+   return 1;
+}
