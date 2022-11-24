71b858230fbad4403c064b22bfb67ec71f82c0a9
AlexVonex <117632525+AlexVonex@users.noreply.github.com>
2022-11-24 12:00:33 +0100

    Create ejercicio5.c

---
+++ Practica 2.2/ejercicio5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+
+int main(){
+    const char path[] = "/home/cursoredes/Practica2/ficheroej5.txt";
+    /*O_RDONLY;
+    O_WRONLY;
+    O_RDWR;*/
+    mode_t properties = 0645;
+    int flags = O_CREAT;
+
+    int abrir = open(path, flags, properties);
+
+    printf("Resultado= %i\n" , abrir);
+    return 0;
