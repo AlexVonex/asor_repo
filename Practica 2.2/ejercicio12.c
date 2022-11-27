70e33bf7f62bc44e76a4244446e9c902fe02d592
AlexVonex <117632525+AlexVonex@users.noreply.github.com>
2022-11-27 13:01:15 +0100

    Create ejercicio12.c

---
+++ Practica 2.2/ejercicio12.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/sysmacros.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+
+	if (argc < 2) {
+		 printf("ERROR: Se debe especeficar la ruta del archivo.\n");
+		 return -1;
+	}
+
+    int fd = open(argv[1], O_CREAT | O_RDWR, 0077);
+   
+    if(fd == -1){
+        printf("ERROR: No se ha podido abrir el fichero.\n");
+		 return -1;
+    }
+    else{
+        int fd2 = dup2(fd, 1);
+
+        printf("Redirigido a %s \n", argv[1]);
+    }
+    return 0;
+
+}
